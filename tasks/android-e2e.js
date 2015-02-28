227219baaa5c122781d5b16e164d71be184203ce
sebv <seb.vincent@gmail.com>
2015-02-28 20:16:39 +0800

    android e2e fix

--- tasks/android-e2e.js
+++ tasks/android-e2e.js
@@ -3,9 +3,9 @@
 var gulp = require('gulp'),
     Q = require('q'),
     runSequence = Q.denodeify(require('run-sequence')),
-    utils = require('../lib/utils');
-    //path = require('path'),
-    //_ = require('underscore');
+    utils = require('../lib/utils'),
+    path = require('path'),
+    _ = require('underscore');
 
 var appiumRoot = global.appiumRoot;
 var argv = global.argv;
@@ -23,24 +23,24 @@ gulp.task('run-android-e2e-worker',
         cwd: appiumRoot
       }).promise;
     }).then(function () {
-      //var env = _.clone(process.env);
-      //env.MOCHA_REPORTER = 'mocha-jenkins-reporter';
-      //env.JUNIT_REPORT_PATH = path.resolve(
-          //global.outputDir,
-          //'report' + ((env.BUILD_NUMBER) ? '-' + env.BUILD_NUMBER : '') +'.xml');
-      //env.JUNIT_REPORT_STACK = 1;
+      var env = _.clone(process.env);
+      env.MOCHA_REPORTER = 'mocha-jenkins-reporter';
+      env.JUNIT_REPORT_PATH = path.resolve(
+          global.outputDir,
+          'report' + ((env.BUILD_NUMBER) ? '-' + env.BUILD_NUMBER : '') +'.xml');
+      env.JUNIT_REPORT_STACK = 1;
 
-      //return utils.smartSpawn('gulp', [
-        //'--color',
-        //'run-android-e2e',
-        //'--avd=' + argv.avd,
-        //'--androidTestSplit=' + argv.androidTestSplit,
-        //'--androidTestGroup=' + argv.androidTestGroup
-      //], {
-        //print: 'Showing test split',
-        //cwd: appiumRoot,
-        //env: env
-      //}).promise;
+      return utils.smartSpawn('gulp', [
+        '--color',
+        'run-android-e2e',
+        '--avd=' + argv.avd,
+        '--androidTestSplit=' + argv.numOfSplits,
+        '--androidTestGroup=' + argv.split
+      ], {
+        print: 'Showing test split',
+        cwd: appiumRoot,
+        env: env
+      }).promise;
     });
 });
 
