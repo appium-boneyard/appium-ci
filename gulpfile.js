4f782cc55a354e5c38fcd08ec17356dd3cd66fbf
sebv <seb.vincent@gmail.com>
2015-05-09 20:50:51 +0800

    ci fix

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ require('./tasks/commons');
 require('./tasks/build');
 require('./tasks/unit-tests');
 require('./tasks/ios-build');
+require('./tasks/ios-e2e');
 require('./tasks/android-build');
 require('./tasks/android-e2e');
 require('./tasks/xunit');
