62ebb6cbe2386cb489df015e151eafb2e0af48e1
sebv <seb.vincent@gmail.com>
2015-05-16 16:53:03 +0800

    fix

--- tasks/xunit.js
+++ tasks/xunit.js
@@ -9,12 +9,12 @@ var gulp = require('gulp'),
 
 gulp.task('collect-downstream-xunit-results', ['prepare-dirs'],function () {
   var uploadServer = process.env.BUILD_UPLOAD_SERVER;
-  var dir = path.join('reports', process.env.JOB_NAME, 'process.env.BUILD_NUMBER');
+  var dir = path.join('reports', process.env.JOB_NAME, process.env.BUILD_NUMBER);
   var inputDir = global.inputDir;
   var reportsDir = global.reportsDir;
   return utils.executeShellCommands([
     'rsync -e \'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\' ' +
-      "'" + 'appium@' + uploadServer + ':' + utils.escapePath(dir) + "/*.tgz' " +
+      "'" + 'appium@' + uploadServer + ':' + '~/' + utils.escapePath(dir) + "/*.tgz' " +
       "'" + inputDir + "'"
     ]).then(function() {
       return utils.executeShellCommands([
