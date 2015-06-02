d4b445cb4f0c38cd9185863177deb9e007e72f0e
sebv <seb.vincent@gmail.com>
2015-06-02 16:43:23 +0800

    fixed instruments command

--- sample-ios-project/lib/list-devices.js
+++ sample-ios-project/lib/list-devices.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import utils from './utils';
 
 async function listDevices() {
-  let [stdout] = await utils.exec('instruments -w devices');
+  let [stdout] = await utils.exec('instruments -s devices');
   var lines = stdout.match(/^.*([\n\r]+|$)/gm);
   return _(lines).filter((l) => {
     return !(l.trim().length === 0 || l.match(/Known Devices/));
