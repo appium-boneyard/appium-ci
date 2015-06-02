b1e967d5a9b61f15f28e33dfc5762001d5367e98
sebv <seb.vincent@gmail.com>
2015-06-02 13:52:50 +0800

    configuring gulp

--- sample-ios-project/gulpfile.js
+++ sample-ios-project/gulpfile.js
@@ -1,6 +1,17 @@
 "use strict";
 
 var gulp = require('gulp'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+    _ = require('lodash'),
+    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp),
+    DEFAULTS = require('appium-gulp-plugins').boilerplate.DEFAULTS;
 
-boilerplate({build: 'sample-ios-project', jscs: false});
+var argv = require('yargs').argv;
+
+boilerplate({
+  build: 'sample-android-project',
+  jscs: false,
+  e2eTest: _.defaults({
+    xCode: true,
+    xCodeVersion: argv.xcodeVersion || '6.1.1'
+  }, DEFAULTS.e2eTest)
+});
