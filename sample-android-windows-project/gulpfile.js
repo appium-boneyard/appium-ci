32dec1d72980a1e64ccd2220d8ce24cc85922e7f
sebv <seb.vincent@gmail.com>
2015-06-16 18:22:32 +0800

    adds sample-android-windows-project subproject

---
+++ sample-android-windows-project/gulpfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var gulp = require('gulp'),
+    _ = require('lodash'),
+    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp),
+    DEFAULTS = require('appium-gulp-plugins').boilerplate.DEFAULTS;
+
+var argv = require('yargs').argv;
+
+boilerplate({
+  build: 'sample-android-windows-project',
+  jscs: false,
+  e2eTest: _.defaults({
+    android: argv.emu,
+    avd: argv.avd
+  }, DEFAULTS.e2eTest)
+});
