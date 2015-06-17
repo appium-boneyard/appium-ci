ab355756b8131b89a0820a0237121586ec278630
sebv <seb.vincent@gmail.com>
2015-06-17 17:50:09 +0800

    configuring powershell

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 echo "Node.js Version: $(node -v)"
 echo "NPM Version: $(npm -v)"
 
-$Env:Path=$Env:Path;C:\Users\appium\AppData\Roaming\npm
+$Env:Path=$Env:Path + ";C:\Users\appium\AppData\Roaming\npm"
 echo "PATH: $Env:Path"
