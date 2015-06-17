24aec7a99b0426d2722b9bf829a33e641347a634
sebv <seb.vincent@gmail.com>
2015-06-17 17:53:22 +0800

    configuring powershell

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,8 @@ echo "NPM Version: $(npm -v)"
 
 $Env:Path=$Env:Path + ";C:\Users\appium\AppData\Roaming\npm"
 echo "PATH: $Env:Path"
+
+npm install -g gulp rimraf
+rimraf node_modules
+npm install
+
