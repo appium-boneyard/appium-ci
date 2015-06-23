db6fc4cbb2e32473accda57b385f7450610e208a
sebv <seb.vincent@gmail.com>
2015-06-23 18:25:02 +0800

    simpler e2e test config

--- sample-android-windows-project/gulpfile.js
+++ sample-android-windows-project/gulpfile.js
@@ -1,16 +1,10 @@
 "use strict";
 
 var gulp = require('gulp'),
-    _ = require('lodash'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp),
-    DEFAULTS = require('appium-gulp-plugins').boilerplate.DEFAULTS;
-
-var argv = require('yargs').argv;
+    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'sample-android-windows-project',
   jscs: false,
-  e2eTest: _.defaults({
-    android: true
-  }, DEFAULTS.e2eTest)
+  e2eTest: { android: true }
 });
