76818d8377a35dbc6c98380000645e0de3848ac9
sebv <seb.vincent@gmail.com>
2015-06-24 14:14:06 +0800

    making sure powershell exit on errors

--- setup.ps1
+++ setup.ps1
@@ -5,9 +5,13 @@ echo "NPM Version: $(npm -v)"
 
 echo "Global npm install..."
 npm --no-color --silent install -g gulp rimraf
+if ($LastExitCode -ne 0) { Exit 1 }
 
 echo "Deleting node_modules..."
 rimraf node_modules
+if ($LastExitCode -ne 0) { Exit 1 }
 
 echo "Installing packages..."
 npm --no-color --silent install
+if ($LastExitCode -ne 0) { Exit 1 }
+
