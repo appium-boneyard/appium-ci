28e1ce99624895e17a893f7631ef86a232b23c3e
Dan Graham <dan@saucelabs.com>
2018-03-27 08:29:54 -0700

    No mocha arrows (#3)

--- test/specs/ios-tool-specs.js
+++ test/specs/ios-tool-specs.js
@@ -10,8 +10,8 @@ import sinon from 'sinon';
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('ios tools', () => {
-  before(async () => {
+describe('ios tools', function () {
+  before(async function () {
     let _exec = utils.exec;
     let _spawn = utils.spawn;
 
@@ -23,35 +23,35 @@ describe('ios tools', () => {
     });
   });
 
-  it('spawn as user',async () => {
+  it('spawn as user',async function () {
     let proc = await iosTools.spawnAsUser('bob', 'ls', ['-l']);
     proc.kill();
   });
 
-  it('spawn as current user',async () => {
+  it('spawn as current user',async function () {
     let proc = await iosTools.spawnAsUser('ls', ['-l']);
     proc.kill();
   });
 
-  it('set simulator scale',async () => {
+  it('set simulator scale',async function () {
     await iosTools.setIosSimulatorScale();
   });
 
-  it('set configure xCode',async () => {
+  it('set configure xCode',async function () {
     await iosTools.configureXcode('6.1.1');
   });
 
-  it('reset simulators',async () => {
+  it('reset simulators',async function () {
     await iosTools.resetSims();
   });
 
-  it('kill all',async () => {
+  it('kill all',async function () {
     await iosTools.killAll();
     await iosTools.killAll('ls');
     await iosTools.killAll(['ls', 'echo']);
   });
 
-  after(async () => {
+  after(async function () {
     utils.exec.restore();
     utils.spawn.restore();
    });
